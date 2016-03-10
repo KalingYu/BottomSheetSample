80e99b53baf674370d5ee238763ac84b610fc4f5
nkdroidsolutions <nkdroidsolutions@gmail.com>
2016-03-10 19:14:01 +0530

    Initial commit

---
+++ app/src/main/java/com/nkdroid/bottomsheetsample/Item.java
@@ -0,0 +1,24 @@
+package com.nkdroid.bottomsheetsample;
+
+
+
+public class Item {
+
+    private int mDrawableRes;
+
+    private String mTitle;
+
+    public Item(int drawable, String title) {
+        mDrawableRes = drawable;
+        mTitle = title;
+    }
+
+    public int getDrawableResource() {
+        return mDrawableRes;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+}
